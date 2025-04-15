d87fc921249f1c13e9eb27adf43a2c07855da0f4
Luvptli <lucxx041@gmail.com>
2025-04-15 15:41:09 +0100

    Empezando UI Lucia

---
+++ Assets/Script/UIBehaviour.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIBehaviour : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            EmpezarJuego();
+        }
+    }
+
+    void EmpezarJuego()
+    {
+
+    }
+}
